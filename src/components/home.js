d1428e33a303b6c0d60a9068918df3768aa34460
Jean <jeanluksp@gmail.com>
2021-11-18 22:12:54 -0300

    Mudança interface Home

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
-import SvgIcon from '@material-ui/core/SvgIcon';
-function Home(props) {
+function Home() {
   return (
-    <SvgIcon {...props}>
-      <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
-    </SvgIcon>
+    <div className="Home">
+      <header className="Home-header">
+        <h1>TESTE INTERFACE HOME</h1>
+        </header>
+    
+    </div>
   );
 }
 export default Home;
