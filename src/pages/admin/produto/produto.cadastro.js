3d1c60fea5de33886a043adbd558de1a9a7f680b
Jean <jeanluksp@gmail.com>
2021-11-23 09:38:00 -0300

    editado formulario cadastro produto

--- src/pages/admin/produto/produto.cadastro.js
+++ src/pages/admin/produto/produto.cadastro.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 
 export default function ProdutoCadastrar() {
   return (
     <div>
       <h1>Cadastro de Produtos</h1>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            id="descricao"
+            name="descricao"
+            label="Descrição"
+            fullWidth
+            autoComplete="given-name"
+            variant="standard"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            id="tamanho"
+            name="tamanho"
+            label="Tamanho"
+            fullWidth
+            autoComplete="given-name"
+            variant="standard"
+          />
+        </Grid>
+      </Grid>
     </div>
   );
 }
--- src/pages/client/produto/produto.details.js
+++ src/pages/client/produto/produto.details.js
@@ -4,7 +4,6 @@ export default function ProdutoDetails() {
   return (
     <div>
       <h1>Detalhes do Produto</h1>
-      
-    </div>
+      </div>
   );
 }
