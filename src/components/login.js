8d8d0a72de3e5484c65d5dfaf39d55798a501cec
Jean <jeanluksp@gmail.com>
2021-11-30 10:05:39 -0300

    ajuste fonte no login

--- src/components/login.js
+++ src/components/login.js
@@ -12,7 +12,7 @@ const Login = () => {
     const avatarStyle = {backgroundColor: '#de61a7'}
     const buttonStyle = {backgroundColor: '#de61a7'}
     const h2Style = {color:'#de61a7'}
-    const h3Style = {color:'#a4949c'}
+    const h3Style = {color:'#a4949c', fontSize:12}
     //const label1 = {color: '#a4949c'}
     
     const history = useHistory();
