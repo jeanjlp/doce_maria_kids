07d0b59c9a7dedcdf772106cc47f1f61fe5c98c9
Jean <jeanluksp@gmail.com>
2021-12-03 02:52:13 -0300

    Pesquisar produtos

--- src/pages/admin/produto/index.js
+++ src/pages/admin/produto/index.js
@@ -15,19 +15,20 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import { ButtonGroup, Button } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 
 const mdTheme = createTheme();
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(descricao, tamanho, valor_compra, valor_venda, quantidade) {
+  return { descricao, tamanho, valor_compra, valor_venda, quantidade };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, ),
-  createData('Ice cream sandwich', 237, 9.0, 37, ),
-  createData('Eclair', 262, 16.0, 24, ),
-  createData('Cupcake', 305, 3.7, 67, ),
-  createData('Gingerbread', 356, 16.0, 49, ),
+  createData('Vestido Rosa','P', 'R$ 35,00', 'R$ 75,00', 2 ),
+  createData('Short Jeans', 'M', 'R$ 40,00', 'R$ 85,00', 3 ),
+  createData('Blusinha Branca', 'G', 'R$ 45,00', 'R$ 95,00', 4),
+  createData('Conjunto Florido', 'P', 'R$ 50,00', 'R$ 105,00', 5),
+  createData('Vestido Lola ursinho', 'M', 'R$ 55,00', 'R$ 110,00', 6),
 ];
 
 export default function ProdutoListagem() {
@@ -60,15 +61,17 @@ export default function ProdutoListagem() {
                   }}
                 >
                   <h2>Listagem de Produtos</h2>
+                  <SearchIcon/>
                   <Grid item xs={12} sm={12}>
                   <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Nome</TableCell>
-            <TableCell align="center">Calories</TableCell>
-            <TableCell align="center">Fat&nbsp;(g)</TableCell>
-            <TableCell align="center">Carbs&nbsp;(g)</TableCell>
+            <TableCell>Descrição</TableCell>
+            <TableCell align="center">Tamanho</TableCell>
+            <TableCell align="center">Valor Compra</TableCell>
+            <TableCell align="center">Valor Venda</TableCell>
+            <TableCell align="center">Quantidade</TableCell>
             <TableCell align="center">Opções</TableCell>
           </TableRow>
         </TableHead>
@@ -79,11 +82,12 @@ export default function ProdutoListagem() {
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {row.name}
+                {row.descricao}
               </TableCell>
-              <TableCell align="center">{row.calories}</TableCell>
-              <TableCell align="center">{row.fat}</TableCell>
-              <TableCell align="center">{row.carbs}</TableCell>
+              <TableCell align="center">{row.tamanho}</TableCell>
+              <TableCell align="center">{row.valor_compra}</TableCell>
+              <TableCell align="center">{row.valor_venda}</TableCell>
+              <TableCell align="center">{row.quantidade}</TableCell>
               <ButtonGroup variant="contained" aria-label="outlined primary button group">
   <Button color ="success">Atualizar</Button>
   <Button color = "error">Excluir</Button>
