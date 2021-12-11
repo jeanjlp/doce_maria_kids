a334fc6254b6c001d626d7c1c3f101ea55c55b7a
Jean <jeanluksp@gmail.com>
2021-12-11 20:16:36 -0300

    istalacao inicial do express

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
  //const { Login } = require("@mui/icons-material");
-(async() => {
-    const db = require("./db");
-    console.log('Começou!');
 
-    console.log('insert into login');
-    const result = await db.insertLogin({usuario:"sherolin", senha:"sherolin"});
-    console.log(result.rowCount);
+// 1 comentar codigo abaixo pra testar com o express
+//  (async() => {
+//     const db = require("./db");
+//     console.log('Começou!');
 
-    console.log('update login');
-    const result2 = await db.updateLogin(1, {usuario : "Sherolin Brunetto", senha: "sherolinbrunetto"});
-    console.log(result2.rowCount);
+//     console.log('insert into login');
+//     const result = await db.insertLogin({usuario:"sherolin", senha:"sherolin"});
+//     console.log(result.rowCount);
 
-    console.log('delete from login');
-    const result3 = await db.deleteLogin(1);
-    console.log(result3.rowCount);
+//     console.log('update login');
+//     const result2 = await db.updateLogin(1, {usuario : "Sherolin Brunetto", senha: "sherolinbrunetto"});
+//     console.log(result2.rowCount);
 
-    console.log('select * from login');
-    const login = await db.selectLogin();
-    console.log(login);
-    })();
+//     console.log('delete from login');
+//     const result3 = await db.deleteLogin(1);
+//     console.log(result3.rowCount);
+
+//     console.log('select * from login');
+//     const login = await db.selectLogin();
+//     console.log(login);
+//     })();
+
+    // 1
+
+    const express = require ('express')
+    const bodyParser = require ('body-parser')
+    const port = 3000
+
+    app.use(bodyParser.json());
+
+    app.use(
+        bodyParser.urlencoded({
+            extended: true,
+        })
+    )
+
+    app.listen(port,()=>{
+        console.log('Servidor rodando na porta de conexão ${port}.')
+    })
